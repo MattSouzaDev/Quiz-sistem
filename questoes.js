4c73bfbeb9a45932da5ef1f746b4e9a6049234e7
RenanSTF <renandasilvateixeirafiladelfio@gmail.com>
2024-10-23 00:00:51 -0400

    Alterado css, e página index, comentado tambem script.js, adcionado questoes em questoes.js

--- questoes.js
+++ questoes.js
@@ -1,34 +1,304 @@
 export default [
+
     {
-      question: "Quanto é 1 + 2?",
+      question: "Quem foi a primeira pessoa a viajar no Espaço?",
       answers: [
-        { option: "3", correct: true },
-        { option: "2", correct: false },
-        { option: "4", correct: false },
+        { option: "Yuri Gagarin", correct: true },
+        { option: "A cadela Laika", correct: false },
+        { option: "Neil Armstrong", correct: false },
+        { option: "Marcos Pontes", correct: false },
+        { option: "Buzz Aldrin", correct: false },
       ],
     },
     {
-      question: "Quanto é 5 x 2?",
+      question: "Qual o plural de chapéu?",
       answers: [
-        { option: "9", correct: false },
-        { option: "10", correct: true },
-        { option: "11", correct: false },
+        { option: "Chapéis", correct: false },
+        { option: "Chapéus", correct: true },
+        { option: "Chapéuzes", correct: false },
+        { option: "Chapuzes", correct: false },
+        { option: "Os chapéu", correct: false },
       ],
     },
     {
-      question: "Quanto é 10 x 8?",
+      question: "Quais as respectivas cores da reciclagem do papel, do vidro, do metal e do plástico?",
       answers: [
-        { option: "800", correct: false },
-        { option: "90", correct: false },
-        { option: "80", correct: true },
+        { option: "azul, verde, amarelo e vermelho", correct: true },
+        { option: "verde, azul, vermelho e amarelo", correct: false },
+        { option: "vermelho, amarelo, verde e azul", correct: false },
+        { option: "azul, amarelo, verde e vermelho", correct: false },
+        { option: "verde, amarelo, azul e vermelho", correct: false },
       ],
     },
     {
-      question: "Quanto é 7 x 7?",
+      question: "O que é um tsunami?",
       answers: [
-        { option: "70", correct: false },
-        { option: "56", correct: false },
-        { option: "49", correct: true },
+        { option: "Um ciclone", correct: false },
+        { option: "Um tornado", correct: false },
+        { option: "Um maremoto", correct: true },
+        { option: "Um terremoto", correct: false },
+        { option: "Um incêndio", correct: false },
       ],
     },
+    {
+      question: "Qual o satélite natural da Terra?",
+      answers: [
+        { option: "SCD-1", correct: false },
+        { option: "Sol", correct: false },
+        { option: "Amazonia 1", correct: false },
+        { option: "Ganimedes", correct: false },
+        { option: "Lua", correct: true },
+      ],
+    },
+    {
+      question: "Qual o nome popular do cloreto de sódio?",
+      answers: [
+        { option: "Vinagre", correct: false },
+        { option: "Água", correct: false },
+        { option: "Papel", correct: false },
+        { option: "Fermento", correct: false },
+        { option: "Sal de cozinha", correct: true },
+      ],
+    },
+    {
+      question: "O que representam os cinco anéis olímpicos?",
+      answers: [
+        { option: "As partes do mundo unidas pelo Olimpismo.", correct: true },
+        { option: "As argolas da ginástica artística.", correct: false },
+        { option: "Cinco tipos de esporte: de rede, de campo, de combate, de precisão, de invasão.", correct: false },
+        { option: "Cinco deuses do Olimpo: Zeus, Atena, Apolo, Dionísio e Hermes.", correct: false },
+        { option: "Os símbolos olímpicos: tocha, hino, mascote, lema e medalhas.", correct: false },
+      ],
+    },
+    {
+      question: "Quais as duas línguas mais faladas no mundo?",
+      answers: [
+        { option: "Inglês e espanhol", correct: false },
+        { option: "Inglês e mandarim chinês", correct: true },
+        { option: "Mandarim chinês e francês", correct: false },
+        { option: "Inglês e português", correct: false },
+        { option: "Inglês e espanhol", correct: false },
+      ],
+    },
+    {
+      question: "Qual o planeta mais próximo do Sol?",
+      answers: [
+        { option: "Netuno", correct: false },
+        { option: "Júpiter", correct: false },
+        { option: "Mercúrio", correct: true },
+        { option: "Terra", correct: false },
+        { option: "Marte", correct: false },
+      ],
+    },
+    {
+      question: "Quantos ossos temos no nosso corpo?",
+      answers: [
+        { option: "126", correct: false },
+        { option: "206", correct: true },
+        { option: "18", correct: false },
+        { option: "300", correct: false },
+        { option: "200", correct: false },
+      ],
+    },  
+    {
+      question: "Quem inventou a lâmpada?",
+      answers: [
+        { option: "Graham Bell", correct: false },
+        { option: "Steve Jobs", correct: false },
+        { option: "Thomas Edison", correct: true },
+        { option: "Henry Ford", correct: false },
+        { option: "Santos Dumont", correct: false },
+      ],
+    },
+    {
+      question: "Qual a montanha mais alta do mundo?",
+      answers: [
+        { option: "Mauna Kea", correct: false },
+        { option: "Dhaulagiri", correct: false },
+        { option: "Monte Chimborazo", correct: false },
+        { option: "Monte Everest", correct: true },
+        { option: "Pico da Neblina", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o menor país do mundo?",
+      answers: [
+        { option: "Mônaco", correct: false },
+        { option: "Vaticano", correct: true },
+        { option: "Ilhas Marshall", correct: false },
+        { option: "Tuvalu", correct: false },
+        { option: "San Marino", correct: false },
+      ],
+    },
+    {
+      question: "Qual das opções abaixo é um exemplo de um adjetivo?",
+      answers: [
+        { option: "Correr", correct: false },
+        { option: "Casa", correct: false },
+        { option: "Feliz", correct: true },
+        { option: "Comer", correct: false },
+        { option: "Azul", correct: true },
+      ],
+    },
+    {
+      question: "Qual é o maior continente em termos de área?",
+      answers: [
+        { option: "África", correct: false },
+        { option: "Europa", correct: false },
+        { option: "Ásia", correct: true },
+        { option: "América do Norte", correct: false },
+        { option: "América do Sul", correct: false },
+      ],
+    },
+    {
+      question: "Quem foi o primeiro presidente dos Estados Unidos?",
+      answers: [
+        { option: "George Washington", correct: true },
+        { option: "Thomas Jefferson", correct: false },
+        { option: "Abraham Lincoln", correct: false },
+        { option: "John Adams", correct: false },
+        { option: "Benjamin Franklin", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o principal gás que compõe a atmosfera terrestre?",
+      answers: [
+        { option: "Oxigênio", correct: false },
+        { option: "Dióxido de carbono", correct: false },
+        { option: "Nitrogênio", correct: true },
+        { option: "Hidrogênio", correct: false },
+        { option: "Argônio", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o valor de 7×(5+3)?",
+      answers: [
+        { option: "42", correct: false },
+        { option: "40", correct: false },
+        { option: "56", correct: true },
+        { option: "50", correct: false },
+        { option: "45", correct: false },
+      ],
+    },
+    {
+      question: "Em que ano foi proclamada a independência do Brasil?",
+      answers: [
+        { option: "1822", correct: true },
+        { option: "1889", correct: false },
+        { option: "1500", correct: false },
+        { option: "1910", correct: false },
+        { option: "1945", correct: false },
+      ],
+    },
+    {
+      question: "Qual parte da célula é responsável pela produção de energia?",
+      answers: [
+        { option: "Núcleo", correct: false },
+        { option: "Ribossomo", correct: false },
+        { option: "Mitocôndria", correct: true },
+        { option: "Lisossomo", correct: false },
+        { option: "Retículo endoplasmático", correct: false },
+      ],
+    },   
+    {
+      question: "Qual é o símbolo químico do ouro?",
+      answers: [
+        { option: "Au", correct: true },
+        { option: "Ag", correct: false },
+        { option: "Fe", correct: false },
+        { option: "Hg", correct: false },
+        { option: "Pb", correct: false },
+      ],
+    },
+    {
+      question: "Qual é a unidade de medida da força no Sistema Internacional?",
+      answers: [
+        { option: "Joule", correct: false },
+        { option: "Watt", correct: false },
+        { option: "Newton", correct: true },
+        { option: "Pascal", correct: false },
+        { option: "Volt", correct: false },
+      ],
+    },
+    {
+      question: "Quem escreveu 'Dom Casmurro'?",
+      answers: [
+        { option: "Machado de Assis", correct: true },
+        { option: "José de Alencar", correct: false },
+        { option: "Jorge Amado", correct: false },
+        { option: "Manuel Bandeira", correct: false },
+        { option: "Graciliano Ramos", correct: false },
+      ],
+    },
+    {
+      question: "Qual é a solução da equação 2x+3=11?",
+      answers: [
+        { option: "x=4", correct: true },
+        { option: "x=3", correct: false },
+        { option: "x=6", correct: false },
+        { option: "x=5", correct: false },
+        { option: "x=7", correct: false },
+      ],
+    },
+    {
+      question: "Qual país possui a maior população do mundo?",
+      answers: [
+        { option: "Índia", correct: false },
+        { option: "Estados Unidos", correct: false },
+        { option: "Indonésia", correct: false },
+        { option: "China", correct: true },
+        { option: "Rússia", correct: false },
+      ],
+    },
+    {
+      question: "Em que ano o Brasil se tornou uma república?",
+      answers: [
+        { option: "1822", correct: false },
+        { option: "1889", correct: true },
+        { option: "1891", correct: false },
+        { option: "1930", correct: false },
+        { option: "1964", correct: false },
+      ],
+    },
+    {
+      question: "Qual país é o maior produtor mundial de café?",
+      answers: [
+        { option: "Vietnã", correct: false },
+        { option: "Colômbia", correct: false },
+        { option: "Indonésia", correct: false },
+        { option: "Brasil", correct: true },
+        { option: "Etiópia", correct: false },
+      ],
+    },
+    {
+      question: "Qual foi o primeiro satélite artificial lançado ao espaço?",
+      answers: [
+        { option: "Sputnik 1", correct: true },
+        { option: "Explorer 1", correct: false },
+        { option: "Luna 2", correct: false },
+        { option: "Vanguard 1", correct: false },
+        { option: "Telstar", correct: false },
+      ],
+    },
+    {
+      question: "Em que ano ocorreu a queda do Muro de Berlim?",
+      answers: [
+        { option: "1980", correct: false },
+        { option: "1985", correct: false },
+        { option: "1989", correct: true },
+        { option: "1991", correct: false },
+        { option: "1993", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o maior deserto do mundo?",
+      answers: [
+        { option: "Deserto da Namíbia", correct: false },
+        { option: "Deserto do Atacama", correct: false },
+        { option: "Deserto do Saara", correct: false },
+        { option: "Deserto da Antártica", correct: true },
+        { option: "Deserto de Gobi", correct: false },
+      ],
+    },                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
   ];
+  
